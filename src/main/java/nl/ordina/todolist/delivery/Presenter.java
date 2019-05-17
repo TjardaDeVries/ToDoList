ecce529af39e8f17536aae342beced9406921b0f
Tjarda de Vries <tjarda.de.vries@ordina.nl>
2019-05-17 10:30:47 +0200

    injects createTaskBoundary and listTasksBounday into the Presenter class and uses them for executing cli commands

--- src/main/java/nl/ordina/todolist/delivery/Presenter.java
+++ src/main/java/nl/ordina/todolist/delivery/Presenter.java
@@ -1,8 +1,21 @@
 package nl.ordina.todolist.delivery;
 
+import nl.ordina.todolist.core.usecases.CreateTaskBoundary;
+import nl.ordina.todolist.core.usecases.ListTasksBoundary;
+
 import java.io.Console;
+import java.util.Date;
 
 public class Presenter {
+
+    final private CreateTaskBoundary createTaskBoundary;
+
+    final private ListTasksBoundary listTasksBoundary;
+
+    public Presenter(final CreateTaskBoundary createTaskBoundary, final ListTasksBoundary listTasksBoundary) {
+        this.createTaskBoundary = createTaskBoundary;
+        this.listTasksBoundary = listTasksBoundary;
+    }
 
     public void start() {
         while (true) {
@@ -11,6 +24,12 @@ public class Presenter {
             switch (command) {
                 case "quit":
                     System.exit(0);
+                case "create":
+                    createTaskBoundary.execute("description", "1", new Date());
+                    break;
+                case "listAll":
+                    listTasksBoundary.execute().forEach(System.out::println);
+                    break;
                 default:
                     System.out.println(command);
             }
