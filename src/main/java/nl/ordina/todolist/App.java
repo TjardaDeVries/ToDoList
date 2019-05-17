3546d89b11cf9ede2069fb1a2d7c8129d8e9c358
Tjarda de Vries <tjarda.de.vries@ordina.nl>
2019-05-17 10:31:25 +0200

    adds the creation of the application objects

--- src/main/java/nl/ordina/todolist/App.java
+++ src/main/java/nl/ordina/todolist/App.java
@@ -1,10 +1,19 @@
 package nl.ordina.todolist;
 
+import nl.ordina.todolist.core.domain.TaskGateway;
+import nl.ordina.todolist.core.usecases.CreateTask;
+import nl.ordina.todolist.core.usecases.CreateTaskBoundary;
+import nl.ordina.todolist.core.usecases.ListTasks;
+import nl.ordina.todolist.core.usecases.ListTasksBoundary;
 import nl.ordina.todolist.delivery.Presenter;
+import nl.ordina.todolist.persistency.TaskRepo;
 
 public class App {
     public static void main(final String[] args) {
-        Presenter presenter = new Presenter();
+        TaskGateway taskGateway = new TaskRepo();
+        CreateTaskBoundary createTaskBoundary = new CreateTask(taskGateway);
+        ListTasksBoundary listTasksBoundary = new ListTasks(taskGateway);
+        Presenter presenter = new Presenter(createTaskBoundary, listTasksBoundary);
         presenter.start();
     }
 }
