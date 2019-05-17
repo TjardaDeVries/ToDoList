453554c875ffda51c5302c69c806fe4f910597d9
Tjarda de Vries <tjarda.de.vries@ordina.nl>
2019-05-17 09:46:21 +0200

    adds domain class Task

---
+++ src/main/java/nl/ordina/todolist/core/domain/Task.java
@@ -0,0 +1,18 @@
+package nl.ordina.todolist.core.domain;
+
+import java.util.Date;
+
+public class Task {
+
+    final private String description;
+
+    final private String priority;
+
+    final private Date dueDate;
+
+    public Task(final String description, final String priority, final Date dueDate) {
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+}
