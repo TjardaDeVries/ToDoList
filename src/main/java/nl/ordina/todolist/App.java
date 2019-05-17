183801fd3e02b2f14ca06f22cea0b909011b2d87
Tjarda de Vries <tjarda.de.vries@ordina.nl>
2019-05-17 08:52:22 +0200

    makes it possible to quit

--- src/main/java/nl/ordina/todolist/App.java
+++ src/main/java/nl/ordina/todolist/App.java
@@ -4,11 +4,15 @@ import java.io.Console;
 
 public class App {
     public static void main(String[] args) {
-
         while (true) {
             Console console = System.console();
-            String command = console.readLine(">: ");
-            System.out.println(command);
+            String command = console.readLine("tdl>: ");
+            switch (command) {
+                case "quit":
+                    System.exit(0);
+                default:
+                    System.out.println(command);
+            }
         }
     }
 }
