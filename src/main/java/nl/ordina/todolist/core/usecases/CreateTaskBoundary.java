251427224df25e69cda0e8bfca7a7940c466c30f
Tjarda de Vries <tjarda.de.vries@ordina.nl>
2019-05-17 14:59:52 +0200

    adds return code to interface and documents it

--- src/main/java/nl/ordina/todolist/core/usecases/CreateTaskBoundary.java
+++ src/main/java/nl/ordina/todolist/core/usecases/CreateTaskBoundary.java
@@ -2,6 +2,13 @@ package nl.ordina.todolist.core.usecases;
 
 public interface CreateTaskBoundary {
 
-    void execute(final String description, final String priority, final String dueDate);
+    /**
+     *
+     * @param description The description of the Task to be created
+     * @param priority The priority of the Task to be created. Must be 1, 2, or 3
+     * @param dueDate The dueDate of the Task to be created. Must be formatted 'dd/MM/yyyy'
+     * @return This method returns a return code: 0 = success, 200 = dueDate could not be parsed
+     */
+    Integer execute(final String description, final String priority, final String dueDate);
 
 }
