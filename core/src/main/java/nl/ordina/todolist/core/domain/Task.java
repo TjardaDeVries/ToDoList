5de82cf5a5caa964ca60163f48829440a42ee200
Tjarda de Vries <>
2019-06-21 16:12:28 +0200

    rewrites Task to a regular PoJo

--- core/src/main/java/nl/ordina/todolist/core/domain/Task.java
+++ core/src/main/java/nl/ordina/todolist/core/domain/Task.java
@@ -1,26 +1,47 @@
 package nl.ordina.todolist.core.domain;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Task {
+public class Task implements Serializable {
 
-    final private String description;
+    private static final long serialVersionUID = 1L;
 
-    final private String priority;
+    private String description;
 
-    final private Date dueDate;
+    private String priority;
 
-    public Task(final String description, final String priority, final Date dueDate) {
-        this.description = description;
-        this.priority = priority;
-        this.dueDate = dueDate;
-    }
+    private Date dueDate;
 
     @Override
     public String toString() {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return String.format("%s (%s) is due on %s", description, priority, dateFormat.format(dueDate));
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public void setPriority(final String priority) {
+        this.priority = priority;
+    }
+
+    public void setDueDate(final Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
     }
 }
