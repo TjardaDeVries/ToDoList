7d4b5d6d714cbd129041c49a69cb9af446e1902f
Tjarda de Vries <>
2019-06-21 16:17:22 +0200

    replaces in-memory version of this repo with a version that reads/writes the list of Tasks to a file (using serialization)

--- persistency/src/main/java/nl/ordina/todolist/persistency/TaskRepo.java
+++ persistency/src/main/java/nl/ordina/todolist/persistency/TaskRepo.java
@@ -3,6 +3,12 @@ package nl.ordina.todolist.persistency;
 import nl.ordina.todolist.core.domain.Task;
 import nl.ordina.todolist.core.domain.TaskGateway;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +16,48 @@ public class TaskRepo implements TaskGateway {
 
     private List<Task> tasks = new ArrayList<>();
 
+    private String file = "/Users/tjardadevries/projects/uwv/lessons/lesson_03/archive/data/tasks";
+
+    public TaskRepo() {
+        if (fileExists()) {
+            loadTasks();
+        }
+    }
+
     public void save(final Task task) {
         tasks.add(task);
+        saveTasks();
     }
 
     public List<Task> retrieveAll() {
         return tasks;
     }
+
+    private void loadTasks() {
+        try (FileInputStream fileInputStream = new FileInputStream(file);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            tasks = (List<Task>) objectInputStream.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveTasks() {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(tasks);
+            objectOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean fileExists() {
+        File tmpDir = new File(file);
+        System.out.println("Persisted tasks found!");
+        return tmpDir.exists();
+    }
+
 }
